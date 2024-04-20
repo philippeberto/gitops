667f71c603edb94ba5f294709c7abf624e2398b6
Philippe Berto <philippe.berto@hexis-hub.com>
2024-04-19 23:53:54 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitops
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("GitOps is funny!"))
+	})
+	http.ListenAndServe(":3000", nil)
+}
